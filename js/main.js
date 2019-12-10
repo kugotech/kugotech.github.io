96093fda09c4434e064253f620d876ec3e034349
Adrian K <adrian.git@kuguar.dev>
2019-12-10 15:55:02 +0100

    Script updated.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 function toggleResponsive() {
+  var c = document.getElementById("topnav-container");
   var m = document.getElementById("topnav");
   var l = document.getElementById("logo");
 
   if (m.className === "topnav") {
+    c.className += " responsive";
     m.className += " responsive";
     l.style.display = "none";
   } else {
+    c.className += "topnav-align";
     m.className = "topnav";
     l.style.display = "block";
   }
